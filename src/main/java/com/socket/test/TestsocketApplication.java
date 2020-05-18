fc3a4e2dac66d6344c37cc74f6a58c8289c478a0
Gabriel Gonçalves <gabriell.gonsalves95@gmail.com>
2020-05-18 18:48:15 -0300

    initial commit

---
+++ src/main/java/com/socket/test/TestsocketApplication.java
@@ -0,0 +1,13 @@
+package com.socket.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsocketApplication.class, args);
+    }
+
+}
